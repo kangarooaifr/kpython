7bdf77c089536db8ac40191c9de4588eb1732e8b
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-03-02 16:15:43 +0100

    Implement requirement file

--- R/pythonConnector_Server.R
+++ R/pythonConnector_Server.R
@@ -20,7 +20,7 @@
 # SERVER LOGIC
 # ------------------------------------------------------------------------------
 
-pythonConnector_Server <- function(id, script_path, script_file = NULL) {
+pythonConnector_Server <- function(id, script_path, script_file = NULL, dependencies = NULL) {
   moduleServer(id, function(input, output, session) {
 
     # -- Get namespace
@@ -32,9 +32,20 @@ pythonConnector_Server <- function(id, script_path, script_file = NULL) {
     # check path
     if(!dir.exists(script_path)) stop('Python script path does not exist! \n')
 
-    # -- Define any Python packages needed for the app here:
-    # TODO: replace by external resource file
-    PYTHON_DEPENDENCIES <- c('pip', 'numpy')
+    # -- Python packages needed for the app:
+    PYTHON_DEPENDENCIES <- NULL
+    if(!is.null(dependencies)){
+
+      target_file <- file.path(dependencies, "python_requirements.txt")
+      cat("[PYTHON] Reading python_requirements file :", target_file, "\n")
+      PYTHON_DEPENDENCIES <- readLines(con <- file(target_file, encoding = "UTF-8"))
+      close(con)
+
+      # *********************** DEBUG ***************************************
+      str(PYTHON_DEPENDENCIES)
+
+
+    }
 
     # -- Get environment variables
     virtualenv_dir <- Sys.getenv('VIRTUALENV_NAME')
@@ -50,9 +61,12 @@ pythonConnector_Server <- function(id, script_path, script_file = NULL) {
 
       # -- Running on Remote Server
       # Create virtual env and install dependencies
+      cat("[PYTHON] Create virtual environment :", virtualenv_dir, "\n")
       reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
+      cat("[PYTHON] Install dependencies :", PYTHON_DEPENDENCIES, "\n")
       reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed=TRUE)
       reticulate::use_virtualenv(virtualenv_dir, required = T)
+      cat("[PYTHON] Environment ready!", PYTHON_DEPENDENCIES, "\n")
 
     } else {
 
