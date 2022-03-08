941313d2552d2d6be3c12a210c3a831531be17bc
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-03-08 12:05:02 +0100

    Adding check with py_available() to avoid conflict

--- R/pythonConnector_Server.R
+++ R/pythonConnector_Server.R
@@ -25,63 +25,70 @@ pythonConnector_Server <- function(id, script_path, script_file = NULL, dependen
 
     # -- Get namespace
     ns <- session$ns
-
-    # log
     cat("[PYTHON] Starting Python Connector Server... \n")
 
-    # check path
+    # -- check path
     if(!dir.exists(script_path)) stop('Python script path does not exist! \n')
 
-    # -- Python packages needed for the app:
-    PYTHON_DEPENDENCIES <- NULL
-    if(!is.null(dependencies)){
+    # -- check if python already initialized
+    if(py_available()){
 
-      target_file <- file.path(dependencies, "python_requirements.txt")
-      cat("[PYTHON] Reading python_requirements file:", target_file, "\n")
-      PYTHON_DEPENDENCIES <- readLines(con <- file(target_file, encoding = "UTF-8"))
-      close(con)
-
-      cat("[PYTHON] Requirements:", PYTHON_DEPENDENCIES, "\n")
-
-    }
-
-    # -- Get environment variables
-    virtualenv_dir <- Sys.getenv('VIRTUALENV_NAME')
-    cat("[PYTHON] VIRTUALENV_NAME:", virtualenv_dir, "\n")
-
-    python_path <- Sys.getenv('PYTHON_PATH')
-    cat("[PYTHON] PYTHON_PATH:", python_path, "\n")
-
-
-    # ----------------------------------------------------------------------------
-    # Python environments setup
-    # ----------------------------------------------------------------------------
-
-    # check user
-    if (Sys.info()[['user']] %in% c('shiny', 'rstudio-connect')){
-
-      # -- Running on Remote Server
-      cat("[PYTHON] Running on Remote Server [user] =", Sys.info()[['user']], "\n")
-
-      # Create virtual env and install dependencies
-      cat("[PYTHON] Create virtual environment :", virtualenv_dir, "\n")
-      reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
-
-      cat("[PYTHON] Install dependencies :", PYTHON_DEPENDENCIES, "\n")
-      reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed=TRUE)
-
-      reticulate::use_virtualenv(virtualenv_dir, required = T)
-      cat("[PYTHON] Environment ready!", virtualenv_dir, "\n")
+      cat("[WARNING] Python is already initialized! \n
+          Which python = ", Sys.which('python'), "\n")
 
     } else {
 
-      # -- Running locally
-      cat("[PYTHON] Running on local machine [user] =", Sys.info()[['user']], "\n")
-      cat("[PYTHON] Working with condaenv =", virtualenv_dir, "\n")
+      # -- Python packages needed for the app:
+      PYTHON_DEPENDENCIES <- NULL
+      if(!is.null(dependencies)){
 
-      # Setup anaconda env
-      use_condaenv(condaenv = virtualenv_dir, conda = "auto", required = FALSE)
+        target_file <- file.path(dependencies, "python_requirements.txt")
+        cat("[PYTHON] Reading python_requirements file:", target_file, "\n")
+        PYTHON_DEPENDENCIES <- readLines(con <- file(target_file, encoding = "UTF-8"))
+        close(con)
 
+        cat("[PYTHON] Requirements:", PYTHON_DEPENDENCIES, "\n")
+
+      }
+
+      # -- Get environment variables
+      virtualenv_dir <- Sys.getenv('VIRTUALENV_NAME')
+      cat("[PYTHON] VIRTUALENV_NAME:", virtualenv_dir, "\n")
+
+      python_path <- Sys.getenv('PYTHON_PATH')
+      cat("[PYTHON] PYTHON_PATH:", python_path, "\n")
+
+
+      # ----------------------------------------------------------------------------
+      # Python environments setup
+      # ----------------------------------------------------------------------------
+
+      # check user
+      if (Sys.info()[['user']] %in% c('shiny', 'rstudio-connect')){
+
+        # -- Running on Remote Server
+        cat("[PYTHON] Running on Remote Server [user] =", Sys.info()[['user']], "\n")
+
+        # Create virtual env and install dependencies
+        cat("[PYTHON] Create virtual environment :", virtualenv_dir, "\n")
+        reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
+
+        cat("[PYTHON] Install dependencies :", PYTHON_DEPENDENCIES, "\n")
+        reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed=TRUE)
+
+        reticulate::use_virtualenv(virtualenv_dir, required = T)
+        cat("[PYTHON] Environment ready!", virtualenv_dir, "\n")
+
+      } else {
+
+        # -- Running locally
+        cat("[PYTHON] Running on local machine [user] =", Sys.info()[['user']], "\n")
+        cat("[PYTHON] Working with condaenv =", virtualenv_dir, "\n")
+
+        # Setup anaconda env
+        use_condaenv(condaenv = virtualenv_dir, conda = "auto", required = FALSE)
+
+      }
     }
 
 
